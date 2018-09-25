5ca46d5fd78432c12f37489758f194c3151fa5e9
independnt <gonzalezdennis6@gmail.com>
2018-09-25 17:50:00 -0400

    added nested routes for products under categories and linked each category to corresponding products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-root 'categories#index'
+  root 'categories#index'
+
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  end
 
 end
